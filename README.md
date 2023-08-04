b6cc05f3f1b09043f602998dc8896b93224985f1
Filip Chalupa <chalupa.filip@gmail.com>
2023-08-04 10:48:54 +0200

    Add basic send

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Czechichat cli
 
 Rozhraní pro komunikaci s [czechichat.deno.dev](https://czechichat.deno.dev/).
+
+## Ukázka použití
+
+```sh
+npx @czechitas/chat "Ahoj světe!"
+```
+
+Odeslanou zprávu si pak můžete prohlédnout na stránkách [czechichat.deno.dev](https://czechichat.deno.dev/).
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello')
+import os from 'os'
+
+const name = os.userInfo().username
+const [_a, _b, message] = process.argv
+
+try {
+	console.log('Odesílá se zpráva:')
+	console.log(`\tJméno: ${name}`)
+	console.log(`\tText: ${message}`)
+	const response = await fetch('https://czechichat.deno.dev/api/send-message', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify({ name, message }),
+	})
+	if (!response.ok || (await response.json()).status !== 'ok') {
+		throw new Error('Response status not ok.')
+	}
+	console.log('Zpráva byla úspěšně odeslána. 🎉')
+} catch (error) {
+	console.error(error)
+	console.error('Odesílání zprávy se nezdařilo. 😵')
+	process.exit(1)
+}
