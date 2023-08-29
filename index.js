542709ce47a463ed6f369d5aa8d374a8a017f76c
Filip Chalupa <chalupa.filip@gmail.com>
2023-08-29 23:54:03 +0200

    Check message

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import { send } from './utilities/send.js'
 const [_a, _b, command, message] = process.argv
 
 if (command === 'send') {
+	if (message === undefined) {
+		console.error(`Chybí zpráva.`)
+		process.exit(2)
+	}
 	await send(message)
 } else if (command === 'list') {
 	await list()
